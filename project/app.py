2cce48df534a598f56f4f1335f45a583ed1d5f95
TubaSpinach <lhtait@gmail.com>
2023-08-15 18:11:00 -0400

    Through login testing

---
+++ project/app.py
@@ -0,0 +1,68 @@
+
+import sqlite3
+from flask import Flask, g, render_template, flash, request, session, redirect, url_for
+
+#config
+DATABASE = "ltpdfs.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "change_me"
+#create/init Flask app
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+def connect_db():
+    rv = sqlite3.connect(app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def get_db():
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
+
+@app.route("/")
+def index():
+    db = get_db()
+    cur = db.execute('select * from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route("/logout")
+def logout():
+    session.pop('logged_in',None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
+
+
+
+if __name__ == "__main__":
+    app.run()
--- tests/app_test.py
+++ tests/app_test.py
@@ -1,9 +1,64 @@
-from project.app import app
+import pytest
+import os
+from pathlib import Path
+
+from project.app import app, init_db
+
+TEST_DB = "test.db"
+
+#pytest uses this function to access testing
+@pytest.fixture
+def client():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    app.config["TESTING"] = True
+    app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
+
+    init_db() #clean database set up
+    yield app.test_client() #tests run
+    init_db() #fresh new database again
+
+#the next two functions help us test auth features
+def login(client, username, password):
+    return client.post(
+        "/login",
+        data=dict(username=username, password=password), 
+        follow_redirects=True,
+    )
+
+def logout(client):
+    return client.get("/logout", follow_redirects=True)
 
 def test_index():
-    tester = app.test_client()
-    response = tester.get("/", content_type="html/text")
-
+    response = client.get("/", content_type="html/text")
     assert response.status_code == 200
-    assert response.data == b"Hello, World!"
 
+def test_database():
+    #first test, to make sure db exists!
+    tester = Path("ltpdfs.db").is_file()
+    assert tester
+
+def test_empty_db(client):
+    rv = client.get("/") #seems like we'll have some observer
+    #                       in "/" that will check the db and display
+    assert b"No entries yet. Add some!" in rv.data
+
+def test_login_logout(client):
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    assert b"You were logged in" in rv.data
+    rv = logout(client)
+    assert b"You were logged out" in rv.data
+    rv = login(client, app.config["USERNAME"] + "x", app.config["PASSWORD"])
+    assert b"Invalid username" in rv.data
+    rv = login(client, app.config["USERNAME"], app.config["PASSWORD"] + "x")
+    assert b"Invalid password" in rv.data
+
+def test_messages(client):
+    login(client, app.config["USERNAME"],app.config["PASSWORD"])
+    rv = client.post(
+        "/add",
+        data=dict(title="<Hello>", text="<strong>HTML</storng> allowed here"),
+        follow_redirects=True,
+    )
+    assert b"No entries here so far" not in rv.data #how does this not conflict with ln 43?
+    assert b"&lt;Hello&gt;" in rv.data
+    assert b"<strong>HTML</strong> allowed here" in rv.data
